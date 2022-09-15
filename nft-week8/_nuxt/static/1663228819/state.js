b75005e0540e925d7b0d214274037915c6dccc9f
andrew781026 <andrew781026@users.noreply.github.com>
2022-09-15 08:04:28 +0000

    deploy: 79750135312c2d42bdfd4eeab5afc8064110ad8c

--- nft-week8/_nuxt/static/1663228819/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcutting-html\u002Fnft-week8\u002F_nuxt\u002Fstatic\u002F1663228819",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcutting-html\u002Fnft-week8\u002F",assetsPath:"\u002Fcutting-html\u002Fnft-week8\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ nft-week8/_nuxt/static/1663229066/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fexplore","\u002F"]})
